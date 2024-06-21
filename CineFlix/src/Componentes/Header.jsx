ce3777d2ea2a7d8134d1621875a94e6e4ca6302c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-21 08:30:31 -0300

    ac

--- CineFlix/src/Componentes/Header.jsx
+++ CineFlix/src/Componentes/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <header className="bg-black text-white p-4 fixed w-full z-10 top-0 shadow-lg">
             <div className="container mx-auto flex items-center justify-between">
                 <div className="flex items-center space-x-4">
-                    <img src="/public/cineflix2.png" alt="Logo" className="h-10"/>
+                    <img src="https://play-lh.googleusercontent.com/WfwkqIpdZz3ABcDVYWY0VPh1YYHGVeezWAEXoQgO-BRRXd7lgu8OwvCwHCAaZnWcFA" alt="Logo" className="h-10"/>
                     <h1 className="text-xl font-bold">CineFlix</h1>
                 </div>
                 <div className="flex items-center space-x-4">
